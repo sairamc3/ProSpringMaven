f2ea440b39e56f29e6fb84f9465a6f3870c8db5e
Sairam Chekuri <sairamc3@gmail.com>
2023-04-14 11:03:54 +0530

    Spring implementation of AOP

---
+++ SpringAop/src/main/java/net/javaguides/SpringAop/SpringAopApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.SpringAop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAopApplication.class, args);
+	}
+
+}
---
+++ SpringAop/src/test/java/net/javaguides/SpringAop/SpringAopApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.SpringAop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
