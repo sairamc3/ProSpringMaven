d177d0753406e6e28ed83c3e0ca5131e99bf3fc7
Sairam Chekuri <sairamc3@gmail.com>
2023-04-15 13:38:57 +0530

    Initial commit for JDBC Learning

---
+++ DbJdbc/src/main/java/com/prospring/DbJdbc/DbJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.prospring.DbJdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbJdbcApplication.class, args);
+	}
+
+}
---
+++ DbJdbc/src/test/java/com/prospring/DbJdbc/DbJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prospring.DbJdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
