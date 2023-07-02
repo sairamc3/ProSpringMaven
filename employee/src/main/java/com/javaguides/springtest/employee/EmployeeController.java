4239c31260741aaf2d858879329dd89d14012393
Sairam Chekuri <sairamc3@gmail.com>
2023-07-02 18:28:36 +0530

    Testing with webclinet

---
+++ employee/src/main/java/com/javaguides/springtest/employee/EmployeeController.java
@@ -0,0 +1,11 @@
+package com.javaguides.springtest.employee;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class EmployeeController {
+    
+    
+    
+}
